5e4ede306f482bebb775f9b47e0fe8e42375ca22
ihorbaiura <ihorbaiura@gmail.com>
2023-06-19 07:54:00 +0300

    feat: create base layout

--- app/src/main/java/com/example/crmappandroid/MainActivity.kt
+++ app/src/main/java/com/example/crmappandroid/MainActivity.kt
@@ -1,43 +1,38 @@
 package com.example.crmappandroid
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.crmappandroid.ui.theme.CRMAppAndroidTheme
+import android.widget.Toast
 
 class MainActivity : ComponentActivity() {
+    private lateinit var textViewTitle: TextView
+    private lateinit var textViewMessage: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CRMAppAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+        textViewTitle = findViewById(R.id.textViewTitle)
+        textViewMessage = findViewById(R.id.textViewMessage)
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
+    fun getProfile(view: View) {
+        val mess = getString(R.string.profile_creating_message)
+        val myToast = Toast.makeText(this, mess, Toast.LENGTH_SHORT)
+        myToast.show()
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CRMAppAndroidTheme {
-        Greeting("Android")
+    fun countMe (view: View) {
+        textViewTitle.text = getString(R.string.score)
+        val countString = textViewMessage.text.toString()
+        var count: Int = countString.toIntOrNull() ?: 0
+        count++
+        textViewMessage.text = count.toString()
+    }
+
+    fun getMessage (view: View) {
+        textViewTitle.text = getString(R.string.greeting)
+        textViewMessage.text = getString(R.string.message)
     }
 }
